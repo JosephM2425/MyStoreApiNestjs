9b166f5c1e6b77cd8402b3a6e37ac18462052bd3
Joseph Murillo <josephmurillo2425@gmail.com>
2022-05-04 00:58:19 -0600

    finish store api nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,14 @@ import { BrandsController } from './controllers/brands/brands.controller';
 import { OrdersController } from './controllers/orders/orders.controller';
 import { CustomersController } from './controllers/customers/customers.controller';
 import { ProductService } from './services/product/product.service';
+import { UsersService } from './services/users/users.service';
+import { BrandsService } from './services/brands/brands.service';
+import { CategoriesService } from './services/categories/categories.service';
+import { CustomersService } from './services/customers/customers.service';
+import { UserService } from './services/user/user.service';
+import { BrandService } from './services/brand/brand.service';
+import { CategoryService } from './services/category/category.service';
+import { CustomerService } from './services/customer/customer.service';
 
 @Module({
   imports: [],
@@ -20,6 +28,6 @@ import { ProductService } from './services/product/product.service';
     OrdersController,
     CustomersController,
   ],
-  providers: [AppService, ProductService],
+  providers: [AppService, ProductService, UsersService, BrandsService, CategoriesService, CustomersService, UserService, BrandService, CategoryService, CustomerService],
 })
 export class AppModule {}
